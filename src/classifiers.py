f0188a64a639c0fe24ca8a842217a3f543a1d4c5
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-02-23 09:28:32 +0000

    feat: define `get_misclassified_examples()` in `classifiers.py`

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.naive_bayes import MultinomialNB  # type: ignore
 from sklearn.linear_model import LogisticRegression  # type: ignore
 from sklearn.metrics import (  # type: ignore
@@ -18,6 +19,23 @@ def evaluate_predictions(y_true, y_pred):
         'F1-Score': f1_score(
             y_true, y_pred, average='weighted', zero_division=0)
     }
+
+
+def get_misclassified_examples(
+        y_true, y_pred, raw_texts, label_encoder, num_examples=5):
+    """
+    Identifies and extracts examples where the model's prediction failed.
+    """
+    errors_df = pd.DataFrame({
+        'Text': raw_texts,
+        'True_Label': label_encoder.inverse_transform(y_true),
+        'Predicted_Label': label_encoder.inverse_transform(y_pred)
+    })
+
+    errors_df = errors_df[
+        errors_df['True_Label'] != errors_df['Predicted_Label']]
+
+    return errors_df.head(num_examples)
 
 
 def train_naive_bayes(X_train, y_train, X_test, y_test):
