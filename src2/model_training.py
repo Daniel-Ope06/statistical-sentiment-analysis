e359dff42364ac46652db739a848d2e0d114455b
Daniel Ope <opeoluwaoyedeji06@gmail.com>
2026-04-09 04:58:35 +0100

    feat: define `get_misclassified_examples()`

--- src2/model_training.py
+++ src2/model_training.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.metrics import (  # type: ignore
     accuracy_score, precision_score, recall_score, f1_score)
 
@@ -16,3 +17,20 @@ def evaluate_predictions(y_true, y_pred):
         'F1-Score': f1_score(
             y_true, y_pred, average='weighted', zero_division=0)
     }
+
+
+def get_misclassified_examples(
+        y_true, y_pred, raw_texts, label_encoder, num_examples=5):
+    """
+    Identifies and extracts examples where the model's prediction failed.
+    """
+    errors_df = pd.DataFrame({
+        'Text': raw_texts,
+        'True_Label': label_encoder.inverse_transform(y_true),
+        'Predicted_Label': label_encoder.inverse_transform(y_pred)
+    })
+
+    errors_df = errors_df[
+        errors_df['True_Label'] != errors_df['Predicted_Label']]
+
+    return errors_df.head(num_examples)
